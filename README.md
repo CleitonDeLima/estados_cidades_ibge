3e963983a98af0615a81c3412f3631e9578bd513
Cleiton de Lima Pinto <cleiton@localhost.localdomain>
2016-07-23 02:09:20 -0300

    First

---
+++ README.md
@@ -0,0 +1,7 @@
+# Estados e Cidades IBGE
+
+Com o arquivo ```base_ibge.csv``` da base de dados do IBGE,
+é gerado um arquivo json com todos os estados e cidades com seus
+respectivos códigos.
+
+Objetivo: Testes ¯\_(ツ)_/¯
---
+++ read_csv_ibge.py
@@ -0,0 +1,53 @@
+import csv
+import json
+
+
+# data = {
+#     "11": {
+#         "name": "Santa Catarina",
+#         "cities": {
+#             "4204202": "Chapecó",,
+#             "9999991": "etc1",
+#             ...
+#         }
+#     },
+#     "10": {
+#         "name": "São Paulo",
+#         "cities": {
+#             "3550308": "São Paulo",
+#             "9999992": "etc2",
+#             ...
+#         }
+#     }
+# }
+
+data = {}
+
+with open('base_ibge.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+
+        code_state = str(row['CodUF'])
+
+        if code_state not in data:
+            state_name = str(row['UF']).capitalize()
+
+            data[code_state] = {}
+            data[code_state].update(
+                dict(
+                    name=state_name,
+                    citys=dict()
+                )
+            )
+
+        city_name = str(row['NomeMunic']).capitalize()
+        code_city = str(row['Codmundv'])
+
+        data[code_state]['citys'].update(
+            {
+                code_city: city_name
+            }
+        )
+
+with open('base_ibge.json', 'w') as json_file:
+    json.dump(data, json_file, ensure_ascii=False, indent=4)
